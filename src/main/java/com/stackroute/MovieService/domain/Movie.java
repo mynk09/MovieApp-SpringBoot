0c02c027387d6cecc5b962977254b5a6e1f071a7
mynk009 <mayank.r@cgi.com>
2019-10-09 10:28:45 +0530

    lombok

--- src/main/java/com/stackroute/MovieService/domain/Movie.java
+++ src/main/java/com/stackroute/MovieService/domain/Movie.java
@@ -3,66 +3,21 @@ package com.stackroute.MovieService.domain;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Movie {
     @Id
     private int movieId;
     private String actorName;
     private String movieComments;
     private String movieName;
-
-
-    public Movie() {
-    }
-
-    public Movie(int movieId, String movieName, String movieComments, String actorName) {
-        this.movieId = movieId;
-        this.actorName=actorName;
-        this.movieComments = movieComments;
-        this.movieName = movieName;
-
-    }
-
-    public int getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
-    }
-
-    public String getActorName() {
-        return actorName;
-    }
-
-    public void setActorName(String actorName) {
-        this.actorName = actorName;
-    }
-
-    public String getMovieComments() {
-        return movieComments;
-    }
-
-    public void setMovieComments(String movieComments) {
-        this.movieComments = movieComments;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "movieId=" + movieId +
-                ", actorName='" + actorName + '\'' +
-                ", movieComments='" + movieComments + '\'' +
-                ", movieName='" + movieName + '\'' +
-                '}';
-    }
 }
 
